1b6ef0fd45e083541999901fd5e74cd1c43c7b90
GSRHackZ <79179978+GSRHackZ@users.noreply.github.com>
2022-09-10 20:11:43 -0400

    Update edgyBot.js

--- edgyBot.js
+++ edgyBot.js
@@ -9,6 +9,7 @@
 // @match        *://auth.edgenuity.com/*
 // @match        *://media.edgenuity.com/media/*
 // @match        *://brainly.com/*
+// @match        https://edgenuity-plus.web.app/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=edgenuity.com
 // @grant GM_setValue
 // @grant GM_getValue
