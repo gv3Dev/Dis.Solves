b303c42ac72fb7ef30e2ff06e54c9cc125c8be24
GSRHackZ <79179978+GSRHackZ@users.noreply.github.com>
2022-09-11 21:44:55 -0400

    Create taction.js
    This is just for the script to gain traction when people search for Edgenuity scripts specifically written in JavaScript. GitHub doesn't seem to understand that the script (obfuscated) is written in JavaScript.

---
+++ taction.js
@@ -0,0 +1,5 @@
+// This is just for the script to gain traction when people search for edgenuity scripts specifically written in javascript
+
+//GitHub doesn't seem to understand that the script (obfuscated) is written in javascript.
+
+alert("This is useless for users but useful for me and people looking for a script 😊")
