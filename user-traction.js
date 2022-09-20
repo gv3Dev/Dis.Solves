ee3ba7f74c93f051af7454a70c72e11f5a3e7ed6
GSRHackZ <79179978+GSRHackZ@users.noreply.github.com>
2022-09-20 15:53:21 -0400

    Update user-traction.js

--- user-traction.js
+++ user-traction.js
@@ -1,5 +1,22 @@
 // This is just for the script to gain traction when people search for edgenuity scripts specifically written in javascript
 
-//GitHub doesn't seem to understand that the script (obfuscated) is written in javascript.
+// GitHub doesn't seem to understand that the script (obfuscated) is written in javascript.
 
-alert("This is useless for users but useful for me and people looking for a script 😊")
+alert("This is useless for users but useful for me and people looking for this script or an edgenuity script. 😊");
+
+// Basically, ignore this file. Go check out edgyBot.js instead. That file contains the actual script. ✔
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// um.....Why are u still here....reading? Please go away...now 🙂
