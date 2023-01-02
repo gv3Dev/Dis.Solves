f04af3bf0027106137ab6355a0161ef832b50906
GSRHackZ <79179978+GSRHackZ@users.noreply.github.com>
2023-01-02 14:55:30 -0500

    Update edgyBot.user.js

--- edgyBot.user.js
+++ edgyBot.user.js
@@ -2,8 +2,8 @@
 // @name         EdgyBot - Edgenuity Bot
 // @namespace    http://tampermonkey.net/
 // @version      2.1973
-// @description  [ Edgy means experimental :) ] - Partially AFK. Answers 99% of activities such as (Intro, Instructions, Warm-ups), Skips past tests and quizzes on course-line in order to quickly get ahead, Unlocks all brainly answers and removes 1 answer per day limit. You can leave it running, but please stay in the same room in case the bot stops. Any problem is fixed witha  simple refresh of the page.
-// @author       EdgePlus+
+// @description  [ Edgy means experimental :) ] - Partially AFK. Answers many activities provided by Edgenuity such as (Intro, Instructions, Warm-ups), Skips past tests and quizzes on course-line in order to quickly get ahead, Unlocks all brainly answers and removes 1 answer per day limit. You can leave it running, but please stay in the same room in case the bot stops. Any problem is fixed witha  simple refresh of the page.
+// @author       EdgePlus / GSRHackZ
 // @match        *://student.edgenuity.com/*
 // @match        *://*.powerspeak.edgenuity.com/*
 // @match        *://*.core.learn.edgenuity.com/*
