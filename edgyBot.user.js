21524a709ec24c33e40447e90e7bec1b76d6ec91
GSRHackZ <79179978+GSRHackZ@users.noreply.github.com>
2023-03-24 21:15:25 -0400

    Update edgyBot.user.js

--- edgyBot.user.js
+++ edgyBot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EdgyBot - Edgenuity Bot
 // @namespace    http://tampermonkey.net/
-// @version      3.27
+// @version      3.271
 // @description  [ Edgy means experimental :) ]
 // @author       EdgePlus / GSRHackZ
 // @match        *://student.edgenuity.com/*
